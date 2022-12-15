6cff596b5d3df802435d2ee8c92ed2896fcfbf43
paulopariz <paulopariz01@gmail.com>
2022-12-15 14:36:18 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="card-imc">
+      <div class="row">
+        <input
+          v-model="massa"
+          type="number"
+          placeholder="Massa"
+          maxlength="10"
+          autofocus
+        />
+      </div>
+      <div class="row">
+        <input
+          v-model="altura"
+          type="number"
+          placeholder="Altura"
+          maxlength="10"
+        />
+      </div>
+      <button class="calc" @click="calcImc">Calcular</button>
+
+      <p>IMC: {{ imc }}</p>
+      <p class=" obs" style="display: none;">OBS: {{ obs }}</p>
+    </div>
+    <tabela-imc></tabela-imc>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabelaImc from './components/TabelaImc.vue';
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { 
+    TabelaImc 
+  },
+  
+  data() {
+     return{
+      massa: "",
+      altura: "",
+      imc: "",
+      obs: "",
+    };
+  },
+  methods: {
+    calcImc() {
+  
+      if(this.massa === "") document.querySelector('.obs').style.display = "block", this.obs = "";
+
+      else if(this.altura === "") this.obs = ""
+   
+
+      this.imc = (this.massa / (this.altura * this.altura)).toFixed(2);
+      this.showObs(this.imc);
+    },
+    showObs(imc) {
+      if (this.imc < 18.5) document.querySelector('.magreza').style.background = "red",
+      document.querySelector('.normal').style.background = "transparent", 
+      document.querySelector('.sobrepeso').style.background = "transparent",
+      document.querySelector('.obesidade').style.background = "transparent",
+      document.querySelector('.obesidadeGrave').style.background = "transparent";
+
+
+      else if (imc < 25) document.querySelector('.normal').style.background = "red",
+      document.querySelector('.magreza').style.background = "transparent",
+      document.querySelector('.sobrepeso').style.background = "transparent",
+      document.querySelector('.obesidade').style.background = "transparent",
+      document.querySelector('.obesidadeGrave').style.background = "transparent";
+
+
+      else if (imc < 30) document.querySelector('.sobrepeso').style.background = "red",
+      document.querySelector('.magreza').style.background = "transparent",
+      document.querySelector('.normal').style.background = "transparent", 
+      document.querySelector('.obesidade').style.background = "transparent",
+      document.querySelector('.obesidadeGrave').style.background = "transparent";
+
+
+      else if (imc < 40) document.querySelector('.obesidade').style.background = "red",
+      document.querySelector('.magreza').style.background = "transparent",
+      document.querySelector('.normal').style.background = "transparent", 
+      document.querySelector('.sobrepeso').style.background = "transparent",
+      document.querySelector('.obesidadeGrave').style.background = "transparent";
+      
+      else if (imc > 40) document.querySelector('.obesidadeGrave').style.background = "red",
+      document.querySelector('.magreza').style.background = "transparent",
+      document.querySelector('.normal').style.background = "transparent", 
+      document.querySelector('.sobrepeso').style.background = "transparent",
+      document.querySelector('.obesidade').style.background = "transparent";
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TabelaImc.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">IMC</th>
+      <th scope="col">CLASSIFICAÇÃO</th>
+      <th scope="col">GRAU DE OBESIDADE</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="magreza">
+      <td>MENOR QUE 18,5</td>
+      <td>MAGREZA</td>
+      <td>0</td>  
+    </tr>
+    <tr class="normal">
+      <td>ENTRE 18,5 E 24,9</td>
+      <td>NORMAL</td>
+      <td>0</td>
+    </tr>
+    <tr class="sobrepeso"> 
+      <td>ENTRE 25,0 E 29,9</td>
+      <td>SOBREPESO</td>
+      <td>I</td>
+    </tr>
+    <tr class="obesidade">
+      <td>ENTRE 30,0 E 39,9</td>
+      <td>OBESIDADE</td>
+      <td>II</td>
+    </tr>
+    <tr class="obesidadeGrave">
+      <td>MAIOR QUE 40,0</td>
+      <td>OBESIDADE GRAVE</td>
+      <td>III</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabelaImc',
+}
+</script>
